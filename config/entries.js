77f6e02c023dc07a6d02458123197064549b91e0
pure-js <evolution.is.already.underway@gmail.com>
2017-04-29 16:35:51 +0300

    App run

--- config/entries.js
+++ config/entries.js
@@ -2,7 +2,6 @@ const path = require('path');
 const root = (__path) => path.join(__dirname, __path);
 
 const entries = {
-    polyfill: root('../demo/polyfill'),
     vendor: root('../demo/vendor'),
     main: root('../demo/main')
 };
